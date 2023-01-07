b51e67c7b7e5f919e797b8d4c173e688633c7678
Stepan Manzurov <mnzsyu@gmail.com>
2023-01-07 10:52:19 +0400

    Remove unused class 'Program'

--- iConverter/Program.cs
+++ iConverter/Program.cs
@@ -1,37 +1,24 @@
-namespace iConverter
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-            }
-
-            app.UseStaticFiles();
-            app.UseRouting();
-
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action=Index}/{id?}");
-
-            app.MapFallbackToFile("index.html"); ;
-
-            app.Run();
-        }
-    }
 }
 
+app.UseStaticFiles();
+app.UseRouting();
 
 
-public partial class Program { }
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
